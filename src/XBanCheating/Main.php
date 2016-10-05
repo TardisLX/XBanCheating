49ebd036267ba79561ff44d90410be332142a33b
雪宸 <16769334@qq.com>
2016-10-05 12:35:33 +0800

    Fix
    开关失效问题，以及美观的注释。

--- src/XBanCheating/Main.php
+++ src/XBanCheating/Main.php
@@ -19,10 +19,10 @@ public function onEnable(){
 		@mkdir($this->path);
 		$this->conf = new Config($this->path."Config.yml", Config::YAML,array(
  				"Enable-XBanCheating"=>"true",
- 				"#总开关"=>"控制XBanCheating是否开启",
+   		  "#1" =>"总开关控制XBanCheating是否开启",
  				"Message"=>"§e创造模式,§c禁止与此物品交互！",
-                                "Ban-Block"=>array(154,118,199),
- 				"#禁止交互列表",
+          "Ban-Block"=>array(154,118,199),
+         "#2" =>"禁止交互的方块ID",
 				));
 	 	$this->getServer()->getPluginManager()->registerEvents($this,$this);
 	  	$this->getLogger()->info("§b SnowXxm(雪宸)§6［贴吧ID: 緑搽丶］§a和 §bMattTradis(塔迪斯)§6［贴吧ID: The_Tradis］§a制作~\n§c仅供测试学习，严禁商业用途");
@@ -32,7 +32,7 @@ public function onEnable(){
  $blockid = $event->getBlock()->getID();
  $banblockid = $this->conf->get("Ban-Block");
  if(in_array($blockid,$banblockid)){
- if($this->conf->get("Enable-XBanCheating") == true){
+ if($this->conf->get("Enable-XBanCheating") == "true"){
  $event->setCancelled(true);
  $message = $this->conf->get("Message");
  $event->getPlayer()->sendMessage("§b［XBanCheating］ $message");
@@ -45,5 +45,3 @@ public function onEnable(){
   
   
 ?>
-
-
