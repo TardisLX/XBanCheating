a7b2790d648d5385f9f5b50362a806ec80c8e7a2
MattTardis <john9322@vip.qq.com>
2016-10-04 20:26:59 -0500

    配置文件应用

--- src/XBanCheating/Main.php
+++ src/XBanCheating/Main.php
@@ -18,19 +18,20 @@ public function onEnable(){
                 $this->path = $this->getDataFolder();
 		@mkdir($this->path);
 				$this->conf = new Config($this->path."Config.yml", Config::YAML,array(
-				"Enable-XBanCheating"=>"true",
-				"#总开关"=>"控制XBanCheating是否开启",
-                                "Ban-Block"=>array(154,118,199),
-				"#配置文件暂时无用",
+                                "message"=>"§b［XBanCheating］§e创造模式§c禁止使用此物品！"
+                                "Ban-Block"=>array(154,118,199)
 				));
 	 	$this->getServer()->getPluginManager()->registerEvents($this,$this);
 	  	$this->getLogger()->info("§c xxm［雪宸］制作，仅供测试学习，禁止商业用途~");
 }
  public function playerBlockTouch(PlayerInteractEvent $event){
  if($event->getPlayer()->getGamemode()==1){
- if($event->getBlock()->getID() == 154 || $event->getBlock()->getID() == 118 || $event->getBlock()->getID() == 199){
+ $blockid = $event->getBlock()->getID();
+ $banblockid = $this->conf->get("Ban-Block");
+ if(in_array($blockid,$banblockid)){
  $event->setCancelled(true);
- $event->getPlayer()->sendMessage("§b［XBanCheating］§e创造模式§c禁止使用此物品！");
+ $message = $this->conf->get("message");
+ $event->getPlayer()->sendMessage($message);
  	}
  	}
      }
