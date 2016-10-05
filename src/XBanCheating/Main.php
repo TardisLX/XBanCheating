89620e89c481ab8ac28d70ab8c049efd8f5da34f
MattTardis <john9322@vip.qq.com>
2016-10-04 21:02:01 -0500

    尝试修复

--- src/XBanCheating/Main.php
+++ src/XBanCheating/Main.php
@@ -15,11 +15,12 @@ class Main extends PluginBase implements Listener{
  private $path,$conf;
 
 public function onEnable(){
-                $this->path = $this->getDataFolder();
+             	$this->path = $this->getDataFolder();
 		@mkdir($this->path);
-				$this->conf = new Config($this->path."Config.yml", Config::YAML,array(
-                                "message"=>"§b［XBanCheating］§e创造模式§c禁止使用此物品！"
-                                "Ban-Block"=>array(154,118,199)
+		$this->conf = new Config($this->path."Config.yml", Config::YAML,array(
+				"admin"=>array()
+				"message"=>"[XBanCheating]创造模式禁止使用此方块！",
+				"item-touch"=>array(118,199,154)
 				));
 	 	$this->getServer()->getPluginManager()->registerEvents($this,$this);
 	  	$this->getLogger()->info("§c xxm［雪宸］制作，仅供测试学习，禁止商业用途~");
